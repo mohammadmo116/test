560c0053a3ec93d545759ae67d6c05193d8ab347
mohammad <mohammad@pop-os.localdomain>
2020-02-05 18:29:28 +0200

    test4

--- softtest.java
+++ softtest.java
@@ -3,7 +3,7 @@ package software;
 public class softtest {
 
 	public static void main(String[] args) {
-		System.out.println("hello world ");
+		System.out.println("hello world hiiiiiiiiiii");
 
 	}	 
 	
