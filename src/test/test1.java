f7a20fa008b94700091c49790643961f9b2aad60
najm_ <najm_@MSI>
2020-02-07 19:24:35 +0200

    testwin2

--- src/test/test1.java
+++ src/test/test1.java
@@ -4,6 +4,11 @@ public class test1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world...");
+		System.out.println("hello world...");
+		System.out.println("hello world...");
+		System.out.println("hello world...");
+		System.out.println("hello world...");
+		System.out.println("hello world...");
 	}
 
 }
