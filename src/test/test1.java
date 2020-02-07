a50ef341682327e138f3f7b1ada5398dfa9f7874
najm_ <najm_@MSI>
2020-02-07 20:54:22 +0200

    last

--- src/test/test1.java
+++ src/test/test1.java
@@ -10,6 +10,7 @@ public class test1 {
 		System.out.println("hello world...");
 		System.out.println("hello world...");
 		System.out.println("hello world...");
+		System.out.println("hello world...");
 	
 	}
 
