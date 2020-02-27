ee00d10051325fb28a3e0b56f70d4c29d0a0efc0
najm_ <najm_@MSI>
2020-02-27 16:44:58 +0200

    test

--- src/Software/Test/test.java
+++ src/Software/Test/test.java
@@ -13,14 +13,16 @@ public class test{
 
 @Test
 	public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber() { 
+	System.out.println("iam in a test method");
 	int a=10;
 	int b =10;
 	Calculator c=new Calculator();
 
 
 	double result=c.sum(a, b);
-Assert.assertTrue(result>50);
+Assert.assertTrue(result>0);
+System.out.println("i finished a test method");
 	}
-	 
+
 	 
  }
